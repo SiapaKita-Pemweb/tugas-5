7eba6c38f1db504c0f14ed83001752c56af3d10a
NamriHolmes-indo <badrinaufal@gmail.com>
2023-05-18 08:46:58 +0700

    first commit

---
+++ calc.js
@@ -0,0 +1,152 @@
+/*
+
+- Project: ITTS Calculator Apps
+- Version: 1.0
+- Last changed: May 17, 2023
+- Assigned to: Group 3 (a.k.a Rey Dylanza, Salman, Thomas Andre, Naufal Amar, Rahmat)
+- Supported: ITTS & Catkay Development Inc.
+- Primary use:  Yes
+
+Copyright 2023 Group 3 (Under licensed PT. Catkay Corporation Tbk.)
+
+https://about.catkay.com/privacy-tos
+
+!!! Anyone, whether permission or without permission, is strictly prohibited from using the entire code included below. !!!
+
+*/
+
+let flag = 0;
+function isNumber(char) {
+  return /^\d$/.test(char);
+}
+
+document.getElementById("answer").readOnly = true;
+let screen = document.getElementById("answer");
+buttons = document.querySelectorAll("button");
+let screenValue = "";
+let maxItems = 6;
+for (item of buttons) {
+  item.addEventListener("click", (e) => {
+    buttonText = e.target.innerText;
+    if (buttonText == "X") {
+      if (flag == 1) {
+        flag = 0;
+      }
+      buttonText = "*";
+      screenValue += buttonText;
+      screen.value = screenValue;
+    } else if (buttonText == "C") {
+      if (flag == 1) {
+        flag = 0;
+      }
+      screenValue = "";
+      screen.value = screenValue;
+    } else if (buttonText == "=") {
+      checkForBracketMulti();
+      
+    } else if (isNumber(buttonText)) {
+      if (flag == 1) {
+        screenValue = buttonText;
+        flag = 0;
+      } else {
+        screenValue += buttonText;
+      }
+      screen.value = screenValue;
+      console.log("Aku adalah operator matematika");
+    } else {
+      if (flag == 1) {
+        flag = 0;
+      }
+      screenValue = screen.value + buttonText;
+      screen.value = screenValue;
+    }
+  });
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.shiftKey == 57) {
+    event.key = "(";
+  } else if (event.shiftKey == 48) {
+    event.key = ")";
+  } else if (event.shiftKey == 53) {
+    event.key = "%";
+  }
+  if (event.keyCode == 88) {
+    screenValue += "*";
+    screen.value = screenValue;
+  }
+  if (
+    event.key <= 9 ||
+    event.key == "+" ||
+    event.key == "-" ||
+    event.key == "*" ||
+    event.key == "." ||
+    event.key == "/" ||
+    event.key == "%" ||
+    event.key == "(" ||
+    event.key == ")"
+  ) {
+    screenValue += event.key;
+    screen.value = screenValue;
+  }
+  if (event.keyCode == 13 || event.keyCode == 187) {
+    checkForBracketMulti(); 
+  } else if (event.keyCode == 46) {
+    screenValue = "";
+    screen.value = screenValue;
+    console.clear();
+  } else if (event.keyCode == 8) {
+    screenValue = screenValue.slice(0, -1);
+    screen.value = screenValue;
+  } else if (event.keyCode == 67) {
+    screenValue = "";
+    screen.value = screenValue;
+    console.clear();
+  } else if (event.keyCode == 82) {
+    location.reload();
+  }
+});
+
+window.onerror = function () {
+  alert("ERROR!!!");
+  screenValue = "";
+  screen.value = screenValue;
+  console.clear();
+};
+
+window.onBracketMultiplication = function () {
+  screenValue = addStr(screen.value, screen.value.indexOf("("), "*");
+  screen.value = eval(screenValue);
+  let calcHistory = JSON.parse(localStorage.getItem("calcHistory")) || [];
+  if(calcHistory.length >= maxItems){
+      calcHistory.shift();
+  }
+  calcHistory.push({screenValue, result : screen.value});
+  localStorage.setItem("calcHistory", JSON.stringify(calcHistory));
+};
+
+function addStr(str, index, stringToAdd) {
+  return (
+    str.substring(0, index) + stringToAdd + str.substring(index, str.length)
+  );
+}
+
+function checkForBracketMulti() {
+
+  if (
+    screen.value.includes("(") &&
+    !isNaN(screen.value.charAt(screen.value.indexOf("(") - 1))
+  ) {
+    window.onBracketMultiplication();
+    return;
+  } else {
+    screen.value = eval(screenValue);
+    let calcHistory = JSON.parse(localStorage.getItem("calcHistory")) || [];
+    if(calcHistory.length >= maxItems){
+        calcHistory.shift();
+    }
+    calcHistory.push({screenValue, result : screen.value});
+    localStorage.setItem("calcHistory", JSON.stringify(calcHistory));
+  }
+  flag = 1;
+}
